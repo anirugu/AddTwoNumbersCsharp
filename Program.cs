9f06d4e56b764b427f2c5ca6edd5a39a6270295a
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-10-22 19:40:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AddTwoNumbersCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+            var result = solution.AddTwoNumbers(l1, l2);
+        }
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,59 @@
+﻿class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        int carry = 0;
+        ListNode l3 = new ListNode();
+        var result = Recursion(l1, l2, l3, carry);
+        return result;
+    }
+
+    public ListNode Recursion(ListNode l1, ListNode l2, ListNode l3, int carry)
+    {
+        if (l1 == null && l2 == null)
+        {
+            if (carry != 0)
+            {
+                l3 = new ListNode();
+                l3.val = carry;
+                return l3;
+            }
+            else
+            {
+                return l3;
+            }
+        }
+        else
+        {
+            l3 = new ListNode();
+        }
+        int result = 0;
+        if (l1 != null && l2 != null)
+        {
+            result = l1.val + l2.val;
+        }
+        else if (l1 != null)
+        {
+            result = l1.val;
+        }
+        else if (l2 != null)
+        {
+            result = l2.val;
+        }
+        result += carry;
+        if (result >= 10)
+        {
+            result = result % 10;
+            carry = 1;
+        }
+        else
+        {
+            carry = 0;
+        }
+        l3 = new ListNode(result);
+        return Recursion(l1.next, l2.next, l3.next, carry);
+    }
+}
+/*
+ #TODO recursion need some fixed to return the result properly.
+ */
