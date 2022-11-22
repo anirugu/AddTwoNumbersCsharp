ccb7abef8e76215f26f23c3ac6c895728f0f61f7
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-11-22 09:00:48 +0530

    saving progress, need to fix runtime error

--- Solution.cs
+++ Solution.cs
@@ -3,11 +3,12 @@
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
         ListNode l3 = new ListNode();
-        ListNode next;
+        ListNode next = l3;
         int carry = 0;
-        while (l1 != null || l2 != null)
+        while (l1 != null || l2 != null || carry != 0)
         {
             int? value = l1?.val + l2?.val + carry;
+            carry = 0;
             if (l1 != null)
                 l1 = l1.next;
             if (l2 != null)
@@ -15,66 +16,12 @@
             if (value > 9)
             {
                 carry = 1;
-                value = value - 10;
+                value = value % 10;
             }
-            l3.
+            var temp = new ListNode(value.Value);
+            next.next = temp;
+            next = next.next;
         }
-        return l3;
+        return l3.next;
     }
-
-    //public ListNode Recursion(ListNode l1, ListNode l2, ListNode l3, int carry)
-    //{
-    //    ListNode l3 = new ListNode();
-
-    //    while (l1 != null || l2 != null)
-    //    {
-    //        int? value = l1?.val + l2?.val;
-    //    }
-
-    //    if (l1 == null && l2 == null)
-    //    {
-    //        if (carry != 0)
-    //        {
-    //            l3 = new ListNode();
-    //            l3.val = carry;
-    //            return l3;
-    //        }
-    //        else
-    //        {
-    //            return l3;
-    //        }
-    //    }
-    //    else
-    //    {
-    //        l3 = new ListNode();
-    //    }
-    //    int result = 0;
-    //    if (l1 != null && l2 != null)
-    //    {
-    //        result = l1.val + l2.val;
-    //    }
-    //    else if (l1 != null)
-    //    {
-    //        result = l1.val;
-    //    }
-    //    else if (l2 != null)
-    //    {
-    //        result = l2.val;
-    //    }
-    //    result += carry;
-    //    if (result >= 10)
-    //    {
-    //        result = result % 10;
-    //        carry = 1;
-    //    }
-    //    else
-    //    {
-    //        carry = 0;
-    //    }
-    //    l3 = new ListNode(result);
-    //    return Recursion(l1.next, l2.next, l3.next, carry);
-    //}
 }
-/*
- #TODO recursion need some fixed to return the result properly.
- */
